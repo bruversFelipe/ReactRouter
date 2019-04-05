fafdc5d23c996a495b66e7e581ff3e82b71da07c
Felipe Bruvers <bruversfelipe@gmail.com>
2019-04-04 23:07:05 -0300

    Remoção functions não usadas

--- reactrouter/src/Routes/index.jsx
+++ reactrouter/src/Routes/index.jsx
@@ -2,30 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import routes from "./routes";
 
-function Sandwiches() {
-  return <h2>Sandwiches</h2>;
-}
-
-function Tacos({ routes }) {
-  return (
-    <div>
-      <h2>Tacos</h2>
-      <ul>
-        <li>
-          <Link to="/tacos/bus">Bus</Link>
-        </li>
-        <li>
-          <Link to="/tacos/cart">Cart</Link>
-        </li>
-      </ul>
-
-      {routes.map((route, i) => (
-        <RenderRoutes key={i} {...route} />
-      ))}
-    </div>
-  );
-}
-
 function RenderRoutes(route) {
   console.log("route", route);
   return (
